9f2719be206b100ac67b9d6b6e35c6276fb95971
StephAlvarado <137136359+StephAlvarado@users.noreply.github.com>
2023-09-21 01:23:54 -0600

    revision final project

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
+//footer
 const today = new Date('2023-08-21');
 let thisYear = today.getFullYear();
 const footer = document.querySelector("footer");
- 
+
+
+//copyright 
 const copyright = document.createElement("p");
 copyright.innerHTML = "Stephanie Alvarado" + ' ' + thisYear;
 footer.appendChild(copyright);
@@ -19,49 +22,80 @@ for (let i = 0; i < skills.length; i++) {
   } 
   
 // Leave a Message
-  const messageForm = document.forms.leave_message;
+  //const messageForm = document.forms.leave_message;
 
-  messageForm.addEventListener("submit", function(event){
-    event.preventDefault();
-
-
-    let usersName = event.target.usersName.value;
-    console.log('Name:',usersName);
-
-    let usersEmail = event.target.usersEmail.value;
-    console.log('Email:', usersEmail);
-
-    let usersMessage = event.target.usersMessage.value;
-    console.log('Message:', usersMessage);
+  //messageForm.addEventListener("submit", function(event) {
+  //  event.preventDefault();
 
 
-    const messageSection = document.getElementById('messages');
-    const messageList = messageSection.querySelector("ul");
-    const newMessage = document.createElement('li');
-    newMessage.innerHTML = `
-    <a href="mailto:${usersEmail}">${usersName}</a>
-    <span>${usersMessage}</span>`
+  //  let usersName = messageForm.target.usersName.value;
+  //  console.log('Name:',usersName);
 
-    messageList.appendChild(newMessage);
+  //  let usersEmail = messageForm.target.usersEmail.value;
+  //  console.log('Email:', usersEmail);
+
+  //  let usersMessage = messageForm.target.usersMessage.value;
+   // console.log('Message:', usersMessage);
+
+
+    ///const messageForm = document.getElementsByName("leave_message")[0];
+    //const messageSection = document.getElementById('messages');
+   /// const messageList = document.querySelector("#messages ul");
+
+   /// messageForm.addEventListener("submit", function(event){
+   ///   event.preventDefault();
+
+    ///  let username= document.getElementsByName(usersName);
+   ///   [0].values;
+   ///   console.log(username);
+   ///   let usersEmail = document.getElementsByName('usersName')[0].value;
+   ///   console.log(usersEmail);
+  ///    let usersMessage = document.getElementsByName("usersMessage")[0].value;
+  ///    console.log(usersMessage);
+
+
+      //adding message
+ ///     const listItem = document.createElement("li");
+ ///     listItem.innerHTML = `<strong>${username}:</strong>
+  ///    ${usersMessage}`;
+  ///    messageList.appendChild(listItem);
+
+   ///   document.getElementsByName("usersName")[0].value = "";
+  ///    document.getElementsByName("usersEmail")[0].value = "";
+   ///   document.getElementsByName("usersMessage")[0].value ="";
+    
+  
+   /// });
+
+
+    //const newMessage = document.createElement('li');
+   // newMessage.innerHTML = `
+   // <a href="mailto:${usersEmail}">${usersName}</a>
+   // <span>${usersMessage}</span>`
+
+   // messageList.appendChild(newMessage);
   
 
-    const removeButton = document.createElement('button');
-    removeButton.innerText = 'Remove';
-    removeButton.type = 'button';
+   // const removeButton = document.createElement('button');
+    //removeButton.innerText = 'Remove';
+   // removeButton.type = 'button';
 
 
-    newMessage.appendChild(removeButton);
-    removeButton.addEventListener('click', function(event) {
+   // newMessage.appendChild(removeButton);
+   // removeButton.addEventListener('click', function(event) {
       
-      const entry = removeButton.parentNode;
-      entry.remove();
-    });
+   //   const entry = removeButton.parentNode;
+   //   entry.remove();
+  //  });
 
     
-messageForm.reset();
-  });
+//messageForm.reset();
 
 
+
+ // });
+
+  
 
 
 // Fetch Data 6.2
