1d25508587c9739b3f6afa962d61158e4f4b49bc
StephAlvarado <137136359+StephAlvarado@users.noreply.github.com>
2023-09-21 23:34:11 -0600

    played around with leave a message section on index.html and index.js still unable to show user message on page

--- js/index.js
+++ js/index.js
@@ -1,107 +1,30 @@
-//footer
+//footer/copyright
 const today = new Date('2023-08-21');
 let thisYear = today.getFullYear();
 const footer = document.querySelector("footer");
 
-
-//copyright 
 const copyright = document.createElement("p");
 copyright.innerHTML = "Stephanie Alvarado" + ' ' + thisYear;
 footer.appendChild(copyright);
 
+
 // Skills Section 
-let skills = ['Microsoft Office', 'Javascript', 'html', 'CSS', 'Lightroom'];
+let skills = ['Microsoft Office', 'Javascript', 'HTML', 'CSS', 'Lightroom'];
 const skillsSection = document.querySelector('#skills');
 const skillsList = skillsSection.querySelector('ul');
 
 for (let i = 0; i < skills.length; i++) {
-   let skill = document.createElement('li');
-   skill.innerText = (skills[i]);
-    skillsList.appendChild(skill);
-    console.log(skills);
+  let skill = document.createElement('li');
+  skill.innerText = (skills[i]);
+  skillsList.appendChild(skill);
+  console.log(skills);
   } 
-  
-// Leave a Message
-  //const messageForm = document.forms.leave_message;
-
-  //messageForm.addEventListener("submit", function(event) {
-  //  event.preventDefault();
 
 
-  //  let usersName = messageForm.target.usersName.value;
-  //  console.log('Name:',usersName);
-
-  //  let usersEmail = messageForm.target.usersEmail.value;
-  //  console.log('Email:', usersEmail);
-
-  //  let usersMessage = messageForm.target.usersMessage.value;
-   // console.log('Message:', usersMessage);
-
-
-    ///const messageForm = document.getElementsByName("leave_message")[0];
-    //const messageSection = document.getElementById('messages');
-   /// const messageList = document.querySelector("#messages ul");
-
-   /// messageForm.addEventListener("submit", function(event){
-   ///   event.preventDefault();
-
-    ///  let username= document.getElementsByName(usersName);
-   ///   [0].values;
-   ///   console.log(username);
-   ///   let usersEmail = document.getElementsByName('usersName')[0].value;
-   ///   console.log(usersEmail);
-  ///    let usersMessage = document.getElementsByName("usersMessage")[0].value;
-  ///    console.log(usersMessage);
-
-
-      //adding message
- ///     const listItem = document.createElement("li");
- ///     listItem.innerHTML = `<strong>${username}:</strong>
-  ///    ${usersMessage}`;
-  ///    messageList.appendChild(listItem);
-
-   ///   document.getElementsByName("usersName")[0].value = "";
-  ///    document.getElementsByName("usersEmail")[0].value = "";
-   ///   document.getElementsByName("usersMessage")[0].value ="";
-    
-  
-   /// });
-
-
-    //const newMessage = document.createElement('li');
-   // newMessage.innerHTML = `
-   // <a href="mailto:${usersEmail}">${usersName}</a>
-   // <span>${usersMessage}</span>`
-
-   // messageList.appendChild(newMessage);
-  
-
-   // const removeButton = document.createElement('button');
-    //removeButton.innerText = 'Remove';
-   // removeButton.type = 'button';
-
-
-   // newMessage.appendChild(removeButton);
-   // removeButton.addEventListener('click', function(event) {
-      
-   //   const entry = removeButton.parentNode;
-   //   entry.remove();
-  //  });
-
-    
-//messageForm.reset();
-
-
-
- // });
-
-  
-
-
-// Fetch Data 6.2
+// Fetch Data 6.2/ projects section
 fetch('https://api.github.com/users/StephAlvarado/repos')
-  .then(response => {
-    if(response.ok){
+ .then(response => {
+   if(response.ok){
       return response.json();
     } else{
       throw new Error('Failed data Fetch from GitHub Api');
@@ -111,9 +34,9 @@ fetch('https://api.github.com/users/StephAlvarado/repos')
  .then(repositories => {
   console.log(repositories);
 
-  //const repositories = JSON.parse(this.response);
-  const projectSection = document.getElementById('projects');
-  const projectlist = projectSection.querySelector("ul");
+
+ const projectSection = document.getElementById('projects');
+ const projectlist = projectSection.querySelector("ul");
   
 
   for (let i = 0; i < repositories.length; i++) {
@@ -133,6 +56,78 @@ fetch('https://api.github.com/users/StephAlvarado/repos')
     console.error("Error fetching data:", error);
 });
 
+// leave a message - section does not 
+const messageForm = document.getElementsByName("leave_message");
+ messageForm.addEventListener("submit", (event) =>{
+    
+    event.preventDefault();
 
-  
+    let usersName = event.target.usersName.value;
+    let usersEmail = event.target.usersEmail.value;
+    let usersMessage = event.target.usersMessage.value; 
+
+    console.log(usersName1);
+    console.log(usersEmail1);
+    console.log(usersMessage1);
+
+    let messageSection = document.getElementById('messages');
+    let messageList = messageSection.querySelector('ul');
+    let newMessage = document.createElement('li');
+    newMessage.innerHTML = `<a href=mailto: ${usersEmail}>${usersName}</a>
+    <span>${usersMessage}</span>`
+
+
+
+
+let removeButton = document.createElement('button');
+  removeButton.innerText = "Remove";
+  removeButton.type= "button";
+
+
+///when I run this code it erases my projects and skills section
+
+
+//removeButton.addEventListener('click',(event) => {
+
+   //let entry = removeButton.parentNode;
+     // entry.remove();
+
+  //newMessage.appendChild(removeButton);
+ // messageList.appendChild(newMessage);
+
+
+    messageForm.reset();
+ })
+
+
+ 
+
+   
+
+
+//
+   //
+
+
+   
+
+
+   //
+   //
+
+
+   
+
+   //let ListSize = document.querySelector('#messages ul').childElementCount;
+   //if (ListSize === 0){
+    //messageHeader.style.display = "none";
+    //} else {
+     //messageHeader.style.display = "";
+    //}
+
+  //});
+
+  //
+
+//});  
 
